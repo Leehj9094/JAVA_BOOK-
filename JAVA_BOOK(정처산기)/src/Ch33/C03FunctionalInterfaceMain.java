8ae620e78e48432b53700c99ae9edb6ed5043641
HyeonJeong <sarann1209@gmail.com>
2025-03-21 17:43:00 +0900

    03.21

--- JAVA_BOOK(정처산기)/src/Ch33/C03FunctionalInterfaceMain.java
+++ JAVA_BOOK(정처산기)/src/Ch33/C03FunctionalInterfaceMain.java
@@ -3,6 +3,9 @@ package Ch33;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import Ch01.C01SystemOut;
 
 //제공되는 함수형 인터페이스들
 //Function<T, R>:
@@ -33,23 +36,48 @@ public class C03FunctionalInterfaceMain {
 	public static Function<List<Integer>, Integer> func3=(list)->{
 		list.stream().reduce(0,(sum,el)->{return sum+el;});return -1;};
 	// 04
-	public static Function<List<Object>, Integer> func4=(list)->{
-		// 1.Integer형만 필터링(instanceof)
+	public static Function<List<Object>, Integer> func4=(li)->{
 		
-		// 누적합 (reduce)
-		return -1;};
+		return li.stream()
+			.filter((item) -> {return item instanceof Integer;})				// 1.Integer형만 필터링(instanceof)
+			.map((item)->{
+//				System.out.println(item);
+				return (Integer)item;})													// 자료형 재구성(Object -> Integer)
+			.reduce(0, (sum,item)->{return 0;});								// 누적합 (reduce)
+		};
 	// 05
-	public static Function<List<Object>, Integer> func5=(list)->{
+	public static Function<List<Object>, List<Integer>> func5=(li)->{
 		// List<Object>로 받은 파라미터 중에 Integer만 추출해서 누적합 구하기
 		// 1.Integer형만 필터링(instanceof)
 		// 2.재구성(map)
 		// 3.리스트로 변환(collect)
-		return -1;};
-		
+		return li.stream()
+				 .filter((item)->item instanceof Integer)
+				 .map((item)->(Integer)item)
+				 .collect(Collectors.toList());
+	};
+	// 06
+	public static Function<Integer, Integer> func6=func1.andThen(func2);
 	
-		
+	// 07
+	public static Function<Integer, Integer> func7=func2.andThen(func1);
+	// 08 
+	public static Function<List<Integer>, Integer> subFunc = (x)->{return x.stream().reduce(1, (sum,x1)->sum+x1);};
+	public static Function<List<Object>,Integer> func8 = func5.andThen(subFunc);
 	
-	public static void main(String[] args) {
+	// 09
+	public static Function <Integer, Function<Integer,Integer>> func9 = (x)->{
+		System.out.println("첫번째 함수 호출! x : " + x);
+		return (y)->{
+			System.out.println("두번째 함수 호출! y : " + y + "x : " + x);
+			return y + x;};};		// 람다식을 계속 반환해야 함
+	// 10
+	public static Function<Integer , Function<Integer, Integer>> func10 = x -> y->x+y;
+	// 11
+	public static Function<Integer , Function<Integer,Function<Integer, Integer>>> func11 = x->y->z->x+y+z;
+	
+	
+	public static void main(String[] args) {	
 		
 		// 01
 		System.out.println(func1.apply(10));
@@ -58,7 +86,19 @@ public class C03FunctionalInterfaceMain {
 		// 03
 		System.out.println(func3.apply(Arrays.asList(10,20,30,40)));
 		// 04
-		
+		System.out.println(func4.apply(Arrays.asList("홍길동",10,20,30,40.5,true)));
+		// 05
+		System.out.println(func5.apply(Arrays.asList("홍길동",10,20,30,40.5,true)));
+		// 06
+		System.out.println(func6.apply(5));
+		// 07
+		System.out.println(func6.apply(5));
+		// 08
+		System.out.println(func5.apply(Arrays.asList("홍길동",10,20,30,40.5,true)));
+		// 09
+		System.out.println(func9.apply(10).apply(20));
+		// 11
+		System.out.println(func11.apply(10).apply(20).apply(30));
 	}
 	
 }
