d025274073570c63986b3fad485afec9b31d3799
HyeonJeong <sarann1209@gmail.com>
2025-04-14 14:44:31 +0900

    0414

---
+++ JSP/09MVC_INIT/src/main/java/Domain/Dao/BookDao.java
@@ -0,0 +1,24 @@
+//package Domain.Dao;
+//
+//import java.sql.SQLException;
+//import java.util.List;
+//
+//import Domain.Dto.UserDto;
+//import Domain.Dto.BookDto;
+//
+//
+//public interface BookDao {
+//
+//	int insert(BookDto bookDto) throws Exception;
+//
+//	int update(UserDto userDto) throws SQLException;
+//
+//	int delete(UserDto userDto) throws SQLException;
+//	//단건조회
+//
+//	UserDto select(UserDto userDto);
+//	//다건조회
+//
+//	List<UserDto> selectAll();
+//
+//}
